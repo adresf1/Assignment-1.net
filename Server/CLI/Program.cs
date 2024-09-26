9978f318ae39d84072fd306645c5cb133f267cbe
adresf <adresfatth@gmail.com>
2024-09-26 10:25:28 +0200

    26/09

--- Server/CLI/Program.cs
+++ Server/CLI/Program.cs
@@ -1,13 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using CLI.UI;
+using FileRepositories;
 using InMemoryRepositories;
 using RepositoryContracts;
 
 Console.WriteLine("Starting...");
-ICommentRepostory commentRepostory = new CommentInMemoryRepository();
-IUserRepository userRepository = new UserInMemoryRepository();
-IPostRepository postRepository = new PostInMemoryRepository();
+ICommentRepostory commentRepostory = new CommentFileRepository();
+IUserRepository userRepository = new UserFileReposity();
+IPostRepository postRepository = new PostFIleReposity();
 
 CliApp cliapp = new CliApp( userRepository, commentRepostory,postRepository );
 
